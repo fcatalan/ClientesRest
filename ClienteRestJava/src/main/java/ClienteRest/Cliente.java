2524142ba8f05a3f6c0c2fb11d0990cb6dba6dad
fcatalan <fabcatalan@gmail.com>
2016-06-26 14:09:00 -0400

    avance metodo get

---
+++ ClienteRestJava/src/main/java/ClienteRest/Cliente.java
@@ -0,0 +1,60 @@
+package ClienteRest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Cliente {
+
+	public Cliente(){}
+	
+	public void metodoGet(){
+		try {
+
+			URL url = new URL("http://jsonplaceholder.typicode.com/posts/1");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
+		  } catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		  } catch (IOException e) {
+
+			e.printStackTrace();
+
+		  }		
+	}
+	
+	public void metodoPost(){
+		
+	}
+	
+	public void metodoPut(){
+		
+	}
+	
+	public void metodoDelete(){
+		
+	}
+}
---
+++ ClienteRestJava/src/main/java/ClienteRest/Main.java
@@ -0,0 +1,12 @@
+package ClienteRest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cliente cliente = new Cliente();
+		
+		cliente.metodoGet();
+	}
+
+}
