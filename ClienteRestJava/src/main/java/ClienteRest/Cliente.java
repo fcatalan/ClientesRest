2243efb1e153c3aac891813abdfc69b8d5b7045e
fcatalan <fabcatalan@gmail.com>
2016-07-04 18:45:17 -0300

    avance de java

--- ClienteRestJava/src/main/java/ClienteRest/Cliente.java
+++ ClienteRestJava/src/main/java/ClienteRest/Cliente.java
@@ -7,14 +7,37 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+
 public class Cliente {
 
+	private final String USER_AGENT = "Mozilla/5.0";
+	
 	public Cliente(){}
+	
+	
 	
 	public void metodoGet(){
 		try {
 
-			URL url = new URL("http://jsonplaceholder.typicode.com/posts/1");
+			URL url = new URL("http://localhost:8080/usuarios");
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
@@ -28,7 +51,7 @@ public class Cliente {
 				(conn.getInputStream())));
 
 			String output;
-			System.out.println("Output from Server .... \n");
+			System.out.println("Salida desde el servidor .... \n");
 			while ((output = br.readLine()) != null) {
 				System.out.println(output);
 			}
@@ -46,15 +69,100 @@ public class Cliente {
 		  }		
 	}
 	
-	public void metodoPost(){
-		
+	public void metodoPost(String nombre, String apellidoP, String apellidoM, String rut, String email, String pass) throws Exception {
+
+		String url = "http://localhost:8080/agregarUsuario";
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(url);
+
+		// add header
+		post.setHeader("User-Agent", USER_AGENT);
+
+		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+		urlParameters.add(new BasicNameValuePair("email", email));
+		urlParameters.add(new BasicNameValuePair("nombre", nombre));
+		urlParameters.add(new BasicNameValuePair("pass", pass));
+		urlParameters.add(new BasicNameValuePair("rut", rut));
+		urlParameters.add(new BasicNameValuePair("apellidoP", apellidoP));
+		urlParameters.add(new BasicNameValuePair("apellidoM", apellidoM));
+
+		post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+		HttpResponse response = client.execute(post);
+		System.out.println("\n Respuesta servidor metodo 'POST' ");		
+		System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		System.out.println(result.toString());
+
 	}
 	
-	public void metodoPut(){
-		
+	public void metodoPut(String nombre, String apellidoP, String apellidoM, String rut, String email, String pass) throws Exception {
+
+		String url = "http://localhost:8080/actualizarUsuario";
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPut put = new HttpPut(url);
+
+		// add header
+		put.setHeader("User-Agent", USER_AGENT);
+
+		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+		urlParameters.add(new BasicNameValuePair("email", email));
+		urlParameters.add(new BasicNameValuePair("nombre", nombre));
+		urlParameters.add(new BasicNameValuePair("pass", pass));
+		urlParameters.add(new BasicNameValuePair("rut", rut));
+		urlParameters.add(new BasicNameValuePair("apellidoP", apellidoP));
+		urlParameters.add(new BasicNameValuePair("apellidoM", apellidoM));
+
+		put.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+		HttpResponse response = client.execute(put);
+		System.out.println("\n Respuesta servidor metodo 'PUT' ");		
+		System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		System.out.println(result.toString());
 	}
 	
-	public void metodoDelete(){
+	public void metodoDelete(String rut) throws Exception {
+
+		String url = "http://localhost:8080/eliminarUsuario/"+rut;
+
+		HttpClient client = new DefaultHttpClient();
+		HttpDelete delete = new HttpDelete(url);
+
+		// add header
+		delete.setHeader("User-Agent", USER_AGENT);
+		delete.setHeader("User-Agent", USER_AGENT);
 		
+		HttpResponse response = client.execute(delete);
+		System.out.println("\n Respuesta servidor metodo 'DELETE' ");		
+		System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		System.out.println(result.toString());
 	}
 }
--- ClienteRestJava/src/main/java/ClienteRest/Main.java
+++ ClienteRestJava/src/main/java/ClienteRest/Main.java
@@ -1,12 +1,106 @@
 package ClienteRest;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method st
+	
+		System.out.println("Escriba la opción que desea realizar \n");
+		System.out.println(" 1.- GET \n 2.- POST \n 3.- PUT \n 4.- DELETE \n");
+		Scanner entrada = new Scanner(System.in);
+		int opcion = Integer.parseInt(entrada.nextLine());
+		
+		System.out.println("Has elegido la opción: "+opcion);
+		
 		Cliente cliente = new Cliente();
 		
-		cliente.metodoGet();
+		
+		String nombre;
+		String apellidoP;
+		String apellidoM;
+		String rut;
+		String email; 
+		String pass;
+		
+		switch (opcion) {
+		case 1:
+			System.out.flush();
+			
+			cliente.metodoGet();
+			break;
+			
+		case 2:
+			System.out.flush();
+			
+			System.out.println("Ha elegido la opción de POST, ingrese los siguientes datos \n");
+			
+			System.out.print("Nombre: ");			
+			nombre = entrada.nextLine();
+			
+			System.out.print("Apellido Paterno: ");			
+			apellidoP = entrada.nextLine();
+			
+			System.out.print("Apellido Materno: ");
+			apellidoM = entrada.nextLine();
+			
+			System.out.print("Rut: ");
+			rut = entrada.nextLine();
+			
+			System.out.print("Email: ");
+			email = entrada.nextLine();
+			
+			System.out.print("Contraseña: ");
+			pass = entrada.nextLine();
+			
+			
+			cliente.metodoPost(nombre, apellidoP, apellidoM, rut, email, pass);
+			break;
+			
+		case 3:
+						
+			System.out.println("Ha elegido la opción de PUT, ingrese los siguientes datos para actualizar a la persona \n");
+			
+			System.out.print("Rut: ");
+			rut = entrada.nextLine();
+			
+			System.out.print("Nombre: ");			
+			nombre = entrada.nextLine();
+			
+			System.out.print("Apellido Paterno: ");			
+			apellidoP = entrada.nextLine();
+			
+			System.out.print("Apellido Materno: ");
+			apellidoM = entrada.nextLine();
+			
+			System.out.print("Email: ");
+			email = entrada.nextLine();
+			
+			System.out.print("Contraseña: ");
+			pass = entrada.nextLine();
+			
+			
+			cliente.metodoPut(nombre, apellidoP, apellidoM, rut, email, pass);
+			
+			break;
+			
+		case 4:
+			
+			System.out.println("Ha elegido la opción de DELETE, el rut de la persona ha eliminar \n");
+			
+			System.out.print("Rut: ");
+			rut = entrada.nextLine();
+					
+			
+			cliente.metodoDelete(rut);
+			
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 
 }
