b9f41fe371f445d8fa7f52607ae6e666c2dfb2d1
fcatalan <fabcatalan@gmail.com>
2016-07-05 18:41:20 -0300

    Cliente C# modo developer

--- ClienteRestMonoDevelop/ClienteRestMonoDevelop/ClienteRest.cs
+++ ClienteRestMonoDevelop/ClienteRestMonoDevelop/ClienteRest.cs
@@ -1,25 +1,209 @@
 ﻿using System;
 using System.Net;
+using System.Text;
+using System.Collections.Generic;
 
 namespace ClienteRestMonoDevelop
 {
 	public class ClienteRest
-	{
+	{		
 		public ClienteRest ()
 		{
 		}
 
-		public void metodoGet()
-		{
-			HttpWebRequest endpointRequest = 
-				(HttpWebRequest)HttpWebRequest.Create("http://localhost:8080");
-			endpointRequest.Method = "GET";
-			endpointRequest.Accept = "application/json;odata=verbose";
 
-			HttpWebResponse endpointResponse =
-				(HttpWebResponse)endpointRequest.GetResponse();
-			Console.WriteLine( endpointResponse.ToString ());
+		public string GetResponse(string urlBase, Dictionary<string, string> parameters, String method)
+		{
+			switch (method)
+			{
+			case "GET":
+				return GetResponse_GET(urlBase, parameters);
+			case "POST":
+				return GetResponse_POST(urlBase, parameters);
+			case "PUT":
+				return GetResponse_PUT(urlBase, parameters);
+			case "DELETE":
+				return GetResponse_DELETE(urlBase, parameters);
+			default:
+				throw new NotImplementedException();
+			}
 		}
+
+
+		private string ConcatParams(Dictionary<string, string> parameters)
+		{
+			bool FirstParam = true;
+			StringBuilder Parametros = null;
+
+			if (parameters != null)
+			{
+				Parametros = new StringBuilder();
+				foreach (KeyValuePair<string, string> param in parameters)
+				{
+					Parametros.Append(FirstParam ? "" : "/");
+					Parametros.Append(param.Key + "=" + param.Value);
+					FirstParam = false;
+				}
+			}
+
+			return Parametros == null ? string.Empty : Parametros.ToString();
+		}
+
+		public string GetResponse_GET(string url, Dictionary<string, string> parameters)
+		{
+			try
+			{
+				//Concatenamos los parametros, OJO: antes del primero debe estar el caracter "?"
+				string parametrosConcatenados = ConcatParams(parameters);
+				string urlConParametros = url + "/" + parametrosConcatenados;
+
+				System.Net.WebRequest wr = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(urlConParametros);
+				wr.Method = "GET";
+
+				wr.ContentType = "application/x-www-form-urlencoded";
+
+				System.IO.Stream newStream;
+				// Obtiene la respuesta
+				System.Net.WebResponse response = wr.GetResponse();
+				// Stream con el contenido recibido del servidor
+				newStream = response.GetResponseStream();
+				System.IO.StreamReader reader = new System.IO.StreamReader(newStream);
+				// Leemos el contenido
+				string responseFromServer = reader.ReadToEnd();
+
+				// Cerramos los streams
+				reader.Close();
+				newStream.Close();
+				response.Close();
+				return responseFromServer;
+			}
+			catch (Exception)
+			{				
+				throw new Exception("Not found remote service " + url);
+			}
+		}
+			
+		public string GetResponse_PUT(string url, Dictionary<string, string> parameters)
+		{
+			try
+			{
+				
+				string parametrosConcatenados = ConcatParams(parameters);
+
+				System.Net.WebRequest wr = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+				wr.Method = "PUT";
+
+				wr.ContentType = "application/x-www-form-urlencoded";
+
+				System.IO.Stream newStream;
+				//Codificación del mensaje
+				System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+				byte[] byte1 = encoding.GetBytes(parametrosConcatenados);
+				wr.ContentLength = byte1.Length;
+				//Envio de parametros
+				newStream = wr.GetRequestStream();
+				newStream.Write(byte1, 0, byte1.Length);
+
+				// Obtiene la respuesta
+				System.Net.WebResponse response = wr.GetResponse();
+				// Stream con el contenido recibido del servidor
+				newStream = response.GetResponseStream();
+				System.IO.StreamReader reader = new System.IO.StreamReader(newStream);
+				// Leemos el contenido
+				string responseFromServer = reader.ReadToEnd();
+
+				// Cerramos los streams
+				reader.Close();
+				newStream.Close();
+				response.Close();
+				return responseFromServer;
+			}
+			catch (Exception)
+			{
+				throw new Exception("Not found remote service " + url);
+			}
+		}
+
+
+		public string GetResponse_POST(string url, Dictionary<string, string> parameters)
+		{
+			try
+			{
+				
+				string parametrosConcatenados = ConcatParams(parameters);
+
+				System.Net.WebRequest wr = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+				wr.Method = "POST";
+
+				wr.ContentType = "application/x-www-form-urlencoded";
+
+				System.IO.Stream newStream;
+				//Codificación del mensaje
+				System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+				byte[] byte1 = encoding.GetBytes(parametrosConcatenados);
+				wr.ContentLength = byte1.Length;
+				//Envio de parametros
+				newStream = wr.GetRequestStream();
+				newStream.Write(byte1, 0, byte1.Length);
+
+				// Obtiene la respuesta
+				System.Net.WebResponse response = wr.GetResponse();
+				// Stream con el contenido recibido del servidor
+				newStream = response.GetResponseStream();
+				System.IO.StreamReader reader = new System.IO.StreamReader(newStream);
+				// Leemos el contenido
+				string responseFromServer = reader.ReadToEnd();
+
+				// Cerramos los streams
+				reader.Close();
+				newStream.Close();
+				response.Close();
+				return responseFromServer;
+			}
+			catch (Exception)
+			{
+				throw new Exception("Not found remote service " + url);
+			}
+		}
+
+
+
+
+		public string GetResponse_DELETE(string url, Dictionary<string, string> parameters)
+		{
+			try
+			{
+				
+				string parametrosConcatenados = ConcatParams(parameters);
+				string urlConParametros = url + "/" + parametrosConcatenados;
+
+
+				System.Net.WebRequest wr = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(urlConParametros);
+				wr.Method = "DELETE";
+
+				wr.ContentType = "application/x-www-form-urlencoded";
+
+				System.IO.Stream newStream;
+				// Obtiene la respuesta
+				System.Net.WebResponse response = wr.GetResponse();
+				// Stream con el contenido recibido del servidor
+				newStream = response.GetResponseStream();
+				System.IO.StreamReader reader = new System.IO.StreamReader(newStream);
+				// Leemos el contenido
+				string responseFromServer = reader.ReadToEnd();
+
+				// Cerramos los streams
+				reader.Close();
+				newStream.Close();
+				response.Close();
+				return responseFromServer;
+			}
+			catch (Exception)
+			{				
+				throw new Exception("Not found remote service " + url);
+			}
+		}
+	  }
 	}
-}
+
 
--- ClienteRestMonoDevelop/ClienteRestMonoDevelop/Program.cs
+++ ClienteRestMonoDevelop/ClienteRestMonoDevelop/Program.cs
@@ -1,14 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClienteRestMonoDevelop
 {
 	class MainClass
 	{
+		public enum Method { GET, POST }
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
 			ClienteRest cliente = new ClienteRest ();
-			cliente.metodoGet ();
+
+
+			Console.WriteLine("Escriba la opción que desea realizar \n");
+			Console.WriteLine(" 1.- GET \n 2.- POST \n 3.- PUT \n 4.- DELETE \n");
+			int opcion = int.Parse(Console.ReadLine ());
+
+			Dictionary<string, string> Parametros = new Dictionary<string,string>();
+
+			switch (opcion) {
+			case 1:
+				Console.Clear ();
+				Console.WriteLine ("Ha elegido la opción de GET \n");
+
+				String respuestaServidor = cliente.GetResponse ("http://localhost:8080/usuarios", Parametros, "GET");
+				Console.WriteLine (respuestaServidor);
+				break;
+
+			case 2:
+				Console.Clear ();
+				Console.WriteLine ("Ha elegido la opción de POST, ingrese los siguientes datos \n");
+
+
+				Console.Write ("Nombre: ");			
+				Parametros.Add ("nombre", Console.ReadLine ());
+
+				Console.Write ("Apellido Paterno: ");			
+				Parametros.Add ("apellidoP", Console.ReadLine ());
+
+				Console.Write ("Apellido Materno: ");
+				Parametros.Add ("apellidoM", Console.ReadLine ());
+
+				Console.Write ("Rut: ");
+				Parametros.Add ("rut", Console.ReadLine ());
+
+				Console.Write ("Email: ");
+				Parametros.Add ("email", Console.ReadLine ());
+
+				Console.Write ("Contraseña: ");
+				Parametros.Add ("pass", Console.ReadLine ());
+
+				Console.Write (cliente.GetResponse ("http://localhost:8080/agregarUsuario", Parametros, "POST"));
+
+				break;
+
+			case 3:
+
+				Console.Clear ();
+				Console.Write ("Ha elegido la opción de PUT, ingrese los siguientes datos para actualizar a la persona \n");
+
+				Console.Write ("Rut: ");
+				Parametros.Add ("rut", Console.ReadLine ());
+
+
+				Console.Write ("Nombre: ");			
+				Parametros.Add ("nombre", Console.ReadLine ());
+
+				Console.Write ("Apellido Paterno: ");			
+				Parametros.Add ("apellidoP", Console.ReadLine ());
+
+				Console.Write ("Apellido Materno: ");
+				Parametros.Add ("apellidoM", Console.ReadLine ());
+
+				Console.Write ("Email: ");
+				Parametros.Add ("email", Console.ReadLine ());
+
+				Console.Write ("Contraseña: ");
+				Parametros.Add ("pass", Console.ReadLine ());
+
+				Console.Write (cliente.GetResponse ("http://localhost:8080/actualizarUsuario", Parametros, "PUT"));
+
+				break;
+
+			case 4:
+
+				Console.WriteLine("Ha elegido la opción de DELETE, el rut de la persona ha eliminar \n");
+
+				Console.WriteLine("Rut: ");
+				Parametros.Add("rut", Console.ReadLine ());
+				//String rut = Console.ReadLine ();
+
+				Console.Write (cliente.GetResponse ("http://localhost:8080/eliminarUsuario", Parametros, "DELETE"));
+
+				break;
+
+			default:
+				break;
+			}
 		}
 	}
 }
